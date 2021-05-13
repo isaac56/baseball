f629e0373715064b93f883c08a0dd56ad699ac23
1song2 <1song2918@gmail.com>
2021-05-13 14:53:58 +0900

    feat: add RecordCellViewModel

---
+++ iOS/BaseballApp/BaseballApp/ViewModel/RecordCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  RecordCellViewModel.swift
+//  BaseballApp
+//
+//  Created by Song on 2021/05/13.
+//
+
+import Foundation
+
+struct RecordCellViewModel {
+    let number: Int
+    let result: String
+    let count: String
+}
