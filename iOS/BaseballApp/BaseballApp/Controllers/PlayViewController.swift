a746eb7c47a4f6128c26cf91a1caedc12186f425
1song2 <1song2918@gmail.com>
2021-05-13 14:25:47 +0900

    feat: modify move animation applying API

--- iOS/BaseballApp/BaseballApp/Controllers/PlayViewController.swift
+++ iOS/BaseballApp/BaseballApp/Controllers/PlayViewController.swift
@@ -58,6 +58,28 @@ class PlayViewController: UIViewController {
             }
             .store(in: &cancelBag)
         
+        viewModel.$isBatterChanged
+            .receive(on: DispatchQueue.main)
+            .zip(viewModel.$game)
+            .sink { [weak self] response in
+                if response.0 {
+                    self?.groundView.move()
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.55) {
+                        self?.groundView.showRunners()
+                        self?.groundView.configureBatterLayer(with: response.1?.data.batter.uniformNumber ?? 0)
+                        self?.groundView.configureRunner1Layer(with: response.1?.data.base1?.uniformNumber)
+                        self?.groundView.configureRunner2Layer(with: response.1?.data.base2?.uniformNumber)
+                        self?.groundView.configureRunner3Layer(with: response.1?.data.base3?.uniformNumber)
+                    }
+                } else {
+                    self?.groundView.configureBatterLayer(with: response.1?.data.batter.uniformNumber ?? 0)
+                    self?.groundView.configureRunner1Layer(with: response.1?.data.base1?.uniformNumber)
+                    self?.groundView.configureRunner2Layer(with: response.1?.data.base2?.uniformNumber)
+                    self?.groundView.configureRunner3Layer(with: response.1?.data.base3?.uniformNumber)
+                }
+            }
+            .store(in: &cancelBag)
+        
         configureUI()
         viewModel.load()
     }
--- iOS/BaseballApp/BaseballApp/Views/GroundView.swift
+++ iOS/BaseballApp/BaseballApp/Views/GroundView.swift
@@ -55,7 +55,14 @@ class GroundView: UIView {
     private var firstBaseLayer = CAShapeLayer()
     private var secondBaseLayer = CAShapeLayer()
     private var thirdBaseLayer = CAShapeLayer()
-    private let runnerLayer = CAShapeLayer()
+    private let batterLayer = CAShapeLayer()
+    private let runner1Layer = CAShapeLayer()
+    private let runner2Layer = CAShapeLayer()
+    private let runner3Layer = CAShapeLayer()
+    private let dummyBatterLayer = CAShapeLayer()
+    private let dummyRunner1Layer = CAShapeLayer()
+    private let dummyRunner2Layer = CAShapeLayer()
+    private let dummyRunner3Layer = CAShapeLayer()
     
     override func awakeFromNib() {
         self.backgroundColor = .systemGray3
@@ -68,7 +75,6 @@ class GroundView: UIView {
         configureInfieldSquareLayer()
         configureHomePlateLayer()
         configureLayerForBases()
-        configureRunnerLayer()
     }
     
     func configure(inningInfo: String) {
@@ -160,20 +166,82 @@ class GroundView: UIView {
         thirdBaseLayer.path = createRhombusPath(for: thirdBaseLayer)
     }
     
-    private func configureRunnerLayer() {
-        runnerLayer.frame = CGRect(x: bounds.midX - Constants.Runner.diameter / 2,
-                                   y: bounds.maxY - Constants.InfieldSquare.padding - (Constants.Runner.diameter / 2),
-                                   width: Constants.Runner.diameter,
-                                   height: Constants.Runner.diameter)
-        layer.addSublayer(runnerLayer)
-        runnerLayer.strokeColor = UIColor.systemGray.cgColor
-        runnerLayer.fillColor = UIColor.systemOrange.cgColor
-        runnerLayer.lineWidth = Constants.Runner.lineWidth
-        let circlePath = UIBezierPath(ovalIn: CGRect(x: runnerLayer.bounds.minX,
-                                                     y: runnerLayer.bounds.minY,
-                                                     width: runnerLayer.bounds.width,
-                                                     height: runnerLayer.bounds.height))
-        runnerLayer.path = circlePath.cgPath
+    func configureBatterLayer(with number: Int) {
+        let position = CGPoint(x: bounds.midX - Constants.Runner.diameter / 2,
+                               y: bounds.maxY - Constants.InfieldSquare.padding - (Constants.Runner.diameter / 2))
+        configureLayer(for: dummyBatterLayer, position: position)
+        configureLayer(for: batterLayer, position: position)
+        updateUniformNumber(for: dummyBatterLayer, with: number)
+        updateUniformNumber(for: batterLayer, with: number)
+    }
+    
+    func configureRunnerLayer(in position: CGPoint, with number: Int?) {
+        configureLayer(for: dummyRunner1Layer, position: position)
+        configureLayer(for: runner1Layer, position: position)
+        updateUniformNumber(for: dummyRunner1Layer, with: number)
+        updateUniformNumber(for: runner1Layer, with: number)
+    }
+    
+    func configureRunner1Layer(with number: Int?) {
+        let position = CGPoint(x: bounds.maxX - Constants.InfieldSquare.padding - Constants.Runner.diameter / 2,
+                              y: bounds.midY - (Constants.Runner.diameter / 2))
+        configureRunnerLayer(in: position, with: number)
+    }
+    
+    func configureRunner2Layer(with number: Int?) {
+        let position = CGPoint(x: bounds.midX - Constants.Runner.diameter / 2,
+                               y: bounds.minY + Constants.InfieldSquare.padding - (Constants.Runner.diameter / 2))
+        configureRunnerLayer(in: position, with: number)
+    }
+    
+    func configureRunner3Layer(with number: Int?) {
+        let position = CGPoint(x: bounds.minX + Constants.InfieldSquare.padding - Constants.Runner.diameter / 2,
+                               y: bounds.midY - (Constants.Runner.diameter / 2))
+        configureRunnerLayer(in: position, with: number)
+    }
+    
+    private func configureLayer(for runner: CAShapeLayer, position: CGPoint) {
+        runner.frame = CGRect(origin: position,
+                              size: CGSize(width: Constants.Runner.diameter,
+                                           height: Constants.Runner.diameter))
+        layer.addSublayer(runner)
+        runner.strokeColor = UIColor.systemGray.cgColor
+        runner.fillColor = UIColor.systemOrange.cgColor
+        runner.lineWidth = Constants.Runner.lineWidth
+        let circlePath = UIBezierPath(ovalIn: CGRect(x: runner.bounds.minX,
+                                                     y: runner.bounds.minY,
+                                                     width: runner.bounds.width,
+                                                     height: runner.bounds.height))
+        runner.path = circlePath.cgPath
+    }
+    
+    func updateUniformNumber(for batterLayer: CAShapeLayer, with number: Int) {
+        batterLayer.sublayers = nil
+        let uniformNumberlayer = CATextLayer()
+        uniformNumberlayer.frame = batterLayer.bounds
+        uniformNumberlayer.fontSize = 20
+        uniformNumberlayer.alignmentMode = .center
+        uniformNumberlayer.string = "\(number)"
+        uniformNumberlayer.truncationMode = .end
+        uniformNumberlayer.foregroundColor = UIColor.black.cgColor
+        batterLayer.addSublayer(uniformNumberlayer)
+    }
+    
+    func updateUniformNumber(for runnerLayer: CAShapeLayer, with number: Int?) {
+        runnerLayer.sublayers = nil
+        guard let number = number else {
+            runnerLayer.isHidden = true
+            return
+        }
+        runnerLayer.isHidden = false
+        let uniformNumberlayer = CATextLayer()
+        uniformNumberlayer.frame = runnerLayer.bounds
+        uniformNumberlayer.fontSize = 20
+        uniformNumberlayer.alignmentMode = .center
+        uniformNumberlayer.string = "\(number)"
+        uniformNumberlayer.truncationMode = .end
+        uniformNumberlayer.foregroundColor = UIColor.black.cgColor
+        runnerLayer.addSublayer(uniformNumberlayer)
     }
     
     private func addCountLayer(nextTo baseLayer: CALayer) -> CAShapeLayer {
@@ -237,15 +305,42 @@ class GroundView: UIView {
     }
     
     @IBAction func pitchButtonPressed(_ sender: UIButton) {
+        delegate?.pitch()
+    }
+    
+    func move() {
+        hideRunners()
+        move(runner: dummyBatterLayer)
+        move(runner: dummyRunner1Layer)
+        move(runner: dummyRunner2Layer)
+        move(runner: dummyRunner3Layer)
+    }
+    
+    func hideRunners() {
+        batterLayer.isHidden = true
+        runner1Layer.isHidden = true
+        runner2Layer.isHidden = true
+        runner3Layer.isHidden = true
+    }
+    
+    func showRunners() {
+        batterLayer.isHidden = false
+        runner1Layer.isHidden = false
+        runner2Layer.isHidden = false
+        runner3Layer.isHidden = false
+    }
+    
+    func move(runner: CAShapeLayer) {
+        runner.isHidden = false
         let homePlatePosition = CGPoint(x: bounds.midX, y: bounds.maxY - Constants.InfieldSquare.padding)
         let firstBasePosition = CGPoint(x: bounds.maxX - Constants.InfieldSquare.padding, y: bounds.midY)
         let secondBasePosition = CGPoint(x: bounds.midX, y: bounds.minY + Constants.InfieldSquare.padding)
         let thirdBasePosition = CGPoint(x: bounds.minX + Constants.InfieldSquare.padding, y: bounds.midY)
-        
-        let initialPosition: CGPoint = runnerLayer.position
+
+        let initialPosition: CGPoint = runner.position
         var finalPosition: CGPoint = CGPoint(x: 0.0, y: 0.0)
-        
-        switch runnerLayer.position {
+    
+        switch runner.position {
         case homePlatePosition:
             finalPosition = firstBasePosition
         case firstBasePosition:
@@ -258,11 +353,17 @@ class GroundView: UIView {
             break
         }
         
-        runnerLayer.position = finalPosition
+        runner.position = finalPosition
+        
+        CATransaction.begin()
+        CATransaction.setCompletionBlock {
+            runner.isHidden = true
+        }
         let animation = CABasicAnimation(keyPath: #keyPath(CALayer.position))
         animation.fromValue = initialPosition
         animation.toValue = finalPosition
-        runnerLayer.animation(forKey: "move")
-        delegate?.pitch()
+        animation.duration = 2.5
+        runner.add(animation, forKey: nil)
+        CATransaction.commit()
     }
 }
